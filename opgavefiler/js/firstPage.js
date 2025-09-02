507a7828694154c7c6420d746a87dec82269130b
eske <eske.larsen@gmail.com>
2025-09-02 09:20:48 +0200

    Completed the codelab

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -1,25 +1,61 @@
 /* Opgave 1*/
+const personName = "Jens Jensen"
+const yearsWorkedInCompany = 23
 
+const message = ` <h2> Hej ${personName} du har nu arbejdet i vores virksomhed i ${yearsWorkedInCompany} år.</h2>`
 
+const container = document.getElementById(`message`)
+container.innerHTML = message
 // din kode her
 
 
-/* Opgave 1*/
+/* Opgave 2*/
+
+const myData = [
+  { navn: "Gifflar", beskrivelse: "En pose af gifflar med kanel", pris: 18 },
+  { navn: "Monster", beskrivelse: "En energidrik med smag af anananas og kokos", pris: 20 },
+  { navn: "Nudler", beskrivelse: "En pakke med instant nudler", pris: 10 }
+];
 
 
-// din kode her
-
-
+const productList = myData.map(item => `
+  <article class="produkt">
+      <h2>${item.navn}</h2>
+      <p>${item.beskrivelse}</p>
+      <p>Pris: ${item.pris} kr.</p>
+  </article>
+`)
+const productListContainer = document.getElementById("produktListe");
+productListContainer.innerHTML = productList;
 
 /* Opgave 3 - skriv videre på koden her: */
+const myForm = document.getElementById('formular');
+const submitButton = document.getElementById('indsend-knap');
+const personList = document.getElementById('person-liste');
 
 // array med person data
 const myPersons = [];
 
 
-// find form elementer
-const myForm = document.getElementById('formular');
-const submitButton = document.getElementById('indsend-knap');
-const personList = document.getElementById('person-liste');
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  // hent data fra form
+  const name = myForm.elements['navn'].value;
+  const age = myForm.elements['alder'].value;
+
+  // tilføj person til array
+  myPersons.push({ navn: name, alder: age });
+    console.log(myPersons);
+
+    personList.innerHTML = '';
+    updatePersonList();
+});
 
 
+// funktion til at opdatere person listen
+function updatePersonList() {
+  personList.innerHTML = myPersons.map(person => `
+    <li>${person.navn} - ${person.alder} år</li>
+  `).join('');
+}
